efa15cfc0b6cad1f3ac152cd0afa2872600e314d
Jan Plastun <undermed@bk.ru>
2025-01-26 16:50:23 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 let swiper;
 function initSwiper() {
-  if (window.innerWidth < 540) {
+  if (window.innerWidth < 576) {
     swiper = new Swiper(".mySwiper", {
       cssMode: true,
       pagination: {
@@ -13,18 +13,12 @@ function initSwiper() {
     });
   }
 }
-function destroySwiper() {
-  if (swiper && window.innerWidth >= 540) {
-    swiper.destroy(true, true);
-    swiper = null;
-  }
-}
-
 initSwiper();
 window.addEventListener("resize", () => {
   initSwiper();
   destroySwiper();
 });
+
 const moreImg = document.querySelector('.more-img');
 document.getElementById('toggleButton').addEventListener('click', function(event) {
   event.preventDefault();  
@@ -45,18 +39,3 @@ document.getElementById('toggleButton').addEventListener('click', function(event
   }
 });
 
-function toggleMenuVisibility() {
-  const swiperMenu = document.querySelector('.mySwiper');
-  const menuMain = document.getElementById('menu-main');
-  const more = document.getElementById('more');
-
-  if (window.innerWidth < 540) {
-    swiperMenu.style.display = 'block';
-    menuMain.style.display = 'none';
-    more.style.display = 'none';
-  } else {
-    swiperMenu.style.display = 'none';
-    menuMain.style.display = 'flex';
-    more.style.display = 'flex';
-  }
-}
